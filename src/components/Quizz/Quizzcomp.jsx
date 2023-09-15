1b6c91c61edec0c2bd8d3c46cd1c077d17a9bdf6
P-1000 <pavan.patchikarla@yahoo.com>
2023-09-15 17:59:44 +0530

    Default commit message

--- src/components/Quizz/Quizzcomp.jsx
+++ src/components/Quizz/Quizzcomp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import quizData from './quizdata';
+import quizData from './Quixx';
 import {motion} from 'framer-motion';
 
 const QuizComponent = () => {
